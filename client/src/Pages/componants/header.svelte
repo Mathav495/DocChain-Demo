d54c9876475f0953f800a47cfb3fc17d2d59a455
GOWTHAMAN R <pudukkottai>
2022-11-23 18:41:35 +0530

    logout route added

--- client/src/Pages/componants/header.svelte
+++ client/src/Pages/componants/header.svelte
@@ -14,7 +14,8 @@
         <p>User1</p>
       </div>
     </div>
-    <div
+    <a
+      href="/"
       class="flex h-10 w-10 items-center justify-center rounded-lg hover:bg-red-700 hover:text-white">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -28,6 +29,6 @@
           stroke-linejoin="round"
           d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
       </svg>
-    </div>
+    </a>
   </div>
 </div>
