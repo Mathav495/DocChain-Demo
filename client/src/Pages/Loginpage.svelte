2977d220e4ed28e938e4aa4ca9536ea652b0fd9e
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-23 14:43:02 +0530

    function created for login submission

--- client/src/Pages/Loginpage.svelte
+++ client/src/Pages/Loginpage.svelte
@@ -1,3 +1,25 @@
+<script>
+  import axios from "axios"
+  let Email, password
+  const onLogin = async () => {
+    console.log(Email)
+    console.log(password)
+
+    let sampleData = {
+      email: Email,
+      password: password,
+    }
+    const { data } = await axios.post(
+      "https://test.swagger.print2block.in/auth/login",
+      sampleData
+    )
+    console.log(data)
+    localStorage.setItem("token", data.token)
+    let token = localStorage.getItem("token")
+    console.log(token)
+  }
+</script>
+
 <div class=" flex h-screen w-screen items-center justify-center bg-black">
   <div class="mx-5 flex w-full flex-col gap-8 md:w-1/2 lg:w-1/4">
     <!-- title -->
@@ -21,16 +43,19 @@
       </h1>
       <div class="flex flex-col gap-5 p-5">
         <input
+          bind:value={Email}
           type="email"
           placeholder="Email address"
           class="w-full rounded-full border  border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200" />
 
         <input
+          bind:value={password}
           type="password"
           placeholder="Password"
           class="w-full rounded-full  border  border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200" />
 
         <button
+          on:click={onLogin}
           class="rounded-l-full rounded-r-full border-0 bg-indigo-500 py-2 px-6 text-lg text-gray-300 hover:bg-indigo-600 focus:outline-none"
           >LOGIN</button>
       </div>
