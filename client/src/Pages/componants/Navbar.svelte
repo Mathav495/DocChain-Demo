96a763ea54242f84ea0f8eb2ddff8872fa59a9dc
GOWTHAMAN R <pudukkottai>
2022-11-23 19:55:48 +0530

    3rd nav added

--- client/src/Pages/componants/Navbar.svelte
+++ client/src/Pages/componants/Navbar.svelte
@@ -35,5 +35,22 @@
       </svg>
       Publish Document
     </a>
+    <a
+      href="Publish"
+      class="flex items-center gap-2 rounded-md border-red-700 px-2 py-2 text-sm font-medium text-white hover:border-l-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="h-6 w-6">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+      </svg>
+      Bulk Upload
+    </a>
   </nav>
 </div>
