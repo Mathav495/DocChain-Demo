d2aa101e8bff28aaca4ddc7831818fe0cbb2787f
GOWTHAMAN R <pudukkottai>
2022-11-23 18:22:20 +0530

    dashboard page designed

--- client/src/Pages/Dashboard.svelte
+++ client/src/Pages/Dashboard.svelte
@@ -1,1 +1,14 @@
-<h1 class="p-5 text-4xl text-gray-500 ">Dashboard page</h1>
+<script>
+import Header from "./componants/Header.svelte"
+import Navbar from "./componants/Navbar.svelte"
+</script>
+
+<div class="h-screen w-screen bg-black text-gray-300">
+  <Header />
+  <div class="flex flex-row">
+    <Navbar />
+    <div class="w-3/4 px-5 py-5 lg:w-5/6">
+      <div class="mb-3 text-xl">Dashboard</div>
+    </div>
+  </div>
+</div>
