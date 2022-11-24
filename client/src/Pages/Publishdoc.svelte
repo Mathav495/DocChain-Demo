7df30ab4dd27e6edff9c83212e5f03d251182245
venkatesh c <venky26494@yahoo.com>
2022-11-24 22:15:25 +0530

    dummy page loader component added

--- client/src/Pages/Publishdoc.svelte
+++ client/src/Pages/Publishdoc.svelte
@@ -1,33 +1,39 @@
 <script>
-  import { navigate } from "svelte-routing"
-  import Header from "../componants/header.svelte"
-  import Navbar from "../componants/Navbar.svelte"
-  let document
+  import { navigate } from "svelte-routing";
+  import Header from "../componants/header.svelte";
+  import Navbar from "../componants/Navbar.svelte";
+
+  let document;
+
   const onsubmit = () => {
-    console.log(document)
+    console.log(document);
     if (document) {
-      navigate(`/user/${document}`)
+      navigate(`/user/${document}`);
     }
-  }
+  };
 </script>
 
 <div class="h-screen w-screen bg-black text-gray-300">
   <Header />
   <div class="flex flex-row">
     <Navbar />
+
     <div class="w-3/4 px-5 py-5 lg:w-5/6">
       <h1 class="mb-3 text-xl">Publish Documents</h1>
       <div>
         <label for="Doc" class="text-lg text-white"
-          >Choose the document to proceed</label>
+          >Choose the document to proceed</label
+        >
         <div class="mt-4 flex gap-5">
           <input
             bind:value={document}
             type="text"
             id="Doc"
-            class="w-2/5 text-lg text-slate-400" />
+            class="w-2/5 text-lg text-slate-400"
+          />
           <button on:click={onsubmit} class="bg-red-500 p-1 text-white"
-            >Proceed</button>
+            >Proceed</button
+          >
         </div>
       </div>
     </div>
