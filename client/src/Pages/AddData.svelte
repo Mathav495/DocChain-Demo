4c479f9e4a3e9e0ef1ff21abf150151292e7198e
MATHAVAN <mathavanselva495@gmail.com>
2022-11-24 21:32:38 +0530

    checked sample file adding

--- client/src/Pages/AddData.svelte
+++ client/src/Pages/AddData.svelte
@@ -2,7 +2,11 @@
   import Header from "../componants/header.svelte"
   import Navbar from "../componants/Navbar.svelte"
   export let document
+  let filename
   console.log(document)
+  const submitfile = () => {
+    console.log(filename)
+  }
 </script>
 
 <div class="h-screen w-screen bg-black text-gray-300">
@@ -11,6 +15,10 @@
     <Navbar />
     <div class="w-3/4 px-5 py-5 lg:w-5/6">
       <h1 class="mb-3 text-xl">Add Data page</h1>
+      <div>
+        <input bind:value={filename} type="file" class="dropify" />
+        <button on:click={submitfile}>Upload</button>
+      </div>
     </div>
   </div>
 </div>
