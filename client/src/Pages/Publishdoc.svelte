e9e087b19b6ddb0025ecc93871d2ece79f0832e8
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-25 12:36:19 +0530

    Document type selected inputbox added

--- client/src/Pages/Publishdoc.svelte
+++ client/src/Pages/Publishdoc.svelte
@@ -18,23 +18,31 @@
 
   <div class="mx-5 flex">
     <Navbar />
-    <div class="mt-2">
-      <h1 class="mt-2 text-2xl font-normal text-slate-400">Publish Document</h1>
-      <h1 class="mt-1 text-lg font-normal text-slate-400">
-        <a href="/Dash" class="cursor-pointer text-blue-500 ">Dashboard</a> / Publish
-        Document
-      </h1>
-    </div>
-    <div class="ml-auto">
-      <button
-        class=" mt-5 rounded bg-indigo-500 px-2 py-1 text-lg text-white hover:bg-indigo-600"
-        >Add Document Type</button>
-      <button
-        class=" mt-5 rounded bg-green-500 px-2 py-1 text-lg text-white hover:bg-green-600"
-        >Edit Document Type</button>
-      <button
-        class=" mt-5 rounded bg-yellow-500 px-2 py-1 text-lg text-white hover:bg-yellow-600"
-        >Edit Recievers</button>
+    <div class="flex w-full flex-col p-5">
+      <div class="flex">
+        <div class="mt-2 flex-1">
+          <h1 class="mt-2 text-2xl font-normal text-slate-400">
+            Publish Document
+          </h1>
+          <h1 class="mt-1 text-lg font-normal text-slate-400">
+            <a href="/Dash" class="cursor-pointer text-blue-500 ">Dashboard</a> /
+            Publish Document
+          </h1>
+        </div>
+      </div>
+
+      <div
+        class="mt-5  w-full  rounded-lg bg-white/10 p-5 shadow-sm shadow-red-600">
+        <h1 class="text-xl text-slate-400">Choose the document to proceed</h1>
+        <div class="mt-4 flex  gap-5">
+          <input
+            type="text"
+            class="w-96 rounded border border-gray-300 bg-black bg-opacity-50 px-2 text-lg leading-8  outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500  focus:ring-2 focus:ring-indigo-200" />
+          <button
+            class=" flex rounded border-0 bg-indigo-500 py-1 px-10 text-lg text-white hover:bg-indigo-600 focus:outline-none"
+            >Proceed</button>
+        </div>
+      </div>
     </div>
   </div>
 </div>
